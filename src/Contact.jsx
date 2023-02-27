b518b1b730a72957a214c08dba22f283d87477f4
Alex Beltz <beltz.a@outlook.com>
2023-02-27 11:35:10 +0800

    Built Contact form, styled, and built framework to submit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function contact() {
-  return <div>contact</div>;
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: handle form submission
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Book Consultation Now</h2>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="contact-input">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="contact-input">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div className="contact-input">
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={phone}
+            onChange={handlePhoneChange}
+          />
+        </div>
+        <div className="contact-input">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleMessageChange}
+          />
+        </div>
+        <button className="contact-submit-btn btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
