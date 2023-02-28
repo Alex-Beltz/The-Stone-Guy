e538f1cfe1d8d14b2f6b38631c97c0858c3b37c2
Alex Beltz <beltz.a@outlook.com>
2023-02-28 09:31:48 +0800

    Refactor Contact component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,6 +5,7 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
+  const [charCount, setCharCount] = useState(0);
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -18,13 +19,39 @@ export default function Contact() {
     setPhone(e.target.value);
   };
 
+  const formatPhoneNumber = (e) => {
+    let phoneNumber = e.target.value.replace(/\D/g, "");
+    if (phoneNumber.length === 10) {
+      phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+    }
+    e.target.value = phoneNumber;
+    setPhone(phoneNumber);
+  };
+
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
+    setCharCount(e.target.value.length);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // TODO: handle form submission
+
+    const nameRegex = /^.{1,50}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneRegex = /^\d{10}$/;
+    const messageRegex = /^.{0,500}$/;
+
+    if (
+      nameRegex.test(name) &&
+      emailRegex.test(email) &&
+      phoneRegex.test(phone) &&
+      messageRegex.test(message)
+    ) {
+      // send email to beltz.a@outlook.com with the submitted information
+      alert("Your message has been submitted!");
+    } else {
+      alert("Please enter valid information in all fields.");
+    }
   };
 
   return (
@@ -34,6 +61,7 @@ export default function Contact() {
         <div className="contact-input">
           <label htmlFor="name">Name:</label>
           <input
+            placeholder="Please type your name"
             type="text"
             id="name"
             name="name"
@@ -44,6 +72,7 @@ export default function Contact() {
         <div className="contact-input">
           <label htmlFor="email">Email:</label>
           <input
+            placeholder="Please type your email"
             type="email"
             id="email"
             name="email"
@@ -54,21 +83,31 @@ export default function Contact() {
         <div className="contact-input">
           <label htmlFor="phone">Phone:</label>
           <input
+            placeholder="Please type your phone #"
             type="tel"
             id="phone"
             name="phone"
             value={phone}
             onChange={handlePhoneChange}
+            onInput={formatPhoneNumber}
           />
         </div>
-        <div className="contact-input">
+        <div className="contact-input" style={{ position: "relative" }}>
           <label htmlFor="message">Message:</label>
           <textarea
+            placeholder="Please type a brief message"
             id="message"
             name="message"
             value={message}
             onChange={handleMessageChange}
           />
+          <span
+            className={`${
+              charCount > 500 ? "char-counter-red" : "char-counter"
+            }`}
+          >
+            {charCount}/500
+          </span>
         </div>
         <button className="contact-submit-btn btn" type="submit">
           Submit
